6e1315b63371c95a6aa36947613d347259e3e123
nakahashi <ryo.nakahashi@gmail.com>
2020-08-25 19:47:30 +0900

    small modify

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -nfs ~/dotfiles/.zshrc ~/.zshrc
 ln -nfs ~/dotfiles/.vimrc ~/.vimrc
+ln -nfs ~/dotfiles/.zsh/.zshrc ~/.zshrc
 rm -rf ~/.config/byobu
 ln -nfs ~/dotfiles/.byobu ~/.config/byobu
